6b6395430eb93f5d821f23589c902444b0f977c0
andres908c <andres908c@gmail.com>
2026-09-01 20:05:05 -0500

    actualizar los archivos

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function mostrarNombres() {
+    const contenedor = document.getElementById("contenedor-nombres");
+    contenedor.innerHTML = `
+        <h3>Integrantes del grupo:</h3>
+        <ul>
+            <li>Daniel Blandón</li>
+            <li>Andrés Juan</li>
+        </ul>
+    `;
+}
 
+document.getElementById("btn-nombres").addEventListener("click", mostrarNombres);
